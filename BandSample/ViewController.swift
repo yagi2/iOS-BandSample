a2791e79cd411e36683fc005a3782246800ab5dc
Itsuki Aoyagi <mail@yagi2.dev>
2021-05-23 22:57:19 +0900

    implement play sound when tapped buttons

--- BandSample/ViewController.swift
+++ BandSample/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // シンバル用の音源ファイルとプレイヤーのインスタンス
+    let cymbalPath = Bundle.main.bundleURL.appendingPathComponent("cymbal.mp3")
+    var cymbalPlayer = AVAudioPlayer()
+    
+    @IBAction func cymbal(_ sender: Any) {
+        do {
+            cymbalPlayer = try AVAudioPlayer(contentsOf: cymbalPath, fileTypeHint: nil)
+            cymbalPlayer.play()
+        } catch {
+            print("シンバル再生でエラーが出ました。")
+        }
+    }
+    
+    // ギター用の音源ファイルとプレイヤーのインsタンス
+    let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
+    var guitarPlayer = AVAudioPlayer()
+    
+    @IBAction func guitar(_ sender: Any) {
+        do {
+            guitarPlayer = try AVAudioPlayer(contentsOf: guitarPath, fileTypeHint: nil)
+            guitarPlayer.play()
+        } catch {
+            print("ギター再生でエラーが出ました。")
+        }
+    }
+    
+    // バックミュージック用の音源ファイルとプレイヤーのインスタンス
+    let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
+    var backmusicPlayer = AVAudioPlayer()
+    
+    @IBAction func backmusicPlay(_ sender: Any) {
+        do {
+            backmusicPlayer = try AVAudioPlayer(contentsOf: backmusicPath, fileTypeHint: nil)
+            backmusicPlayer.numberOfLoops = -1 // 無限ループでリピート再生する
+            backmusicPlayer.play()
+        } catch {
+            print("バックミュージック再生でエラーが出ました")
+        }
+    }
+    
+    @IBAction func backmusicStop(_ sender: Any) {
+        if (backmusicPlayer.isPlaying) {
+            backmusicPlayer.stop()
+        }
+    }
 }
 
